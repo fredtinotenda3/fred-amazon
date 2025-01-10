6e71f2dae285c9fa610bd8a05a5a42e86149cc63
Tinotenda Fred <fredtinotenda3@gail.com>
2025-01-10 22:22:51 +0200

    implement: 34-admin-web-pages

--- types/index.ts
+++ types/index.ts
@@ -1,15 +1,21 @@
 import {
+  CarouselSchema,
   CartSchema,
+  DeliveryDateSchema,
   OrderInputSchema,
   OrderItemSchema,
+  PaymentMethodSchema,
   ProductInputSchema,
   ReviewInputSchema,
   SettingInputSchema,
   ShippingAddressSchema,
+  SiteCurrencySchema,
+  SiteLanguageSchema,
   UserInputSchema,
   UserNameSchema,
   UserSignInSchema,
   UserSignUpSchema,
+  WebPageInputSchema,
 } from "@/lib/validator";
 import { z } from "zod";
 
@@ -21,13 +27,13 @@ export type IReviewDetails = IReviewInput & {
     name: string;
   };
 };
-
 export type IProductInput = z.infer<typeof ProductInputSchema>;
+
 export type Data = {
   settings: ISettingInput[];
+  webPages: IWebPageInput[];
   users: IUserInput[];
   products: IProductInput[];
-
   reviews: {
     title: string;
     rating: number;
@@ -45,8 +51,7 @@ export type Data = {
     isPublished: boolean;
   }[];
 };
-
-// ORDER
+// Order
 export type IOrderInput = z.infer<typeof OrderInputSchema>;
 export type IOrderList = IOrderInput & {
   _id: string;
@@ -66,6 +71,16 @@ export type IUserSignIn = z.infer<typeof UserSignInSchema>;
 export type IUserSignUp = z.infer<typeof UserSignUpSchema>;
 export type IUserName = z.infer<typeof UserNameSchema>;
 
-// setting
+// webpage
+export type IWebPageInput = z.infer<typeof WebPageInputSchema>;
 
+// setting
+export type ICarousel = z.infer<typeof CarouselSchema>;
 export type ISettingInput = z.infer<typeof SettingInputSchema>;
+export type ClientSetting = ISettingInput & {
+  currency: string;
+};
+export type SiteLanguage = z.infer<typeof SiteLanguageSchema>;
+export type SiteCurrency = z.infer<typeof SiteCurrencySchema>;
+export type PaymentMethod = z.infer<typeof PaymentMethodSchema>;
+export type DeliveryDate = z.infer<typeof DeliveryDateSchema>;
